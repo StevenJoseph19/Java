c0f2423a71d5c9c061ace25b4eb7bf69bad95419
Stephen Samuels <stevesam1927@gmail.com>
2023-08-08 00:17:55 +0530

    Updated Day 6 supporting Materials

--- Day 6/Supporting Materials/3/Application.java
+++ Day 6/Supporting Materials/3/Application.java
@@ -5,9 +5,9 @@ import java.sql.*;
 public class Application {
 
 	public static void main(String[] args) throws SQLException  {
-		String connectionUrl = "jdbc:sqlserver://localhost:1434;databaseName=loboticket;integratedSecurity=true;";
-
-		try (Connection con = DriverManager.getConnection(connectionUrl);) {
+//		String connectionUrl = "jdbc:sqlserver://localhost:1434;databaseName=loboticket;integratedSecurity=true;";
+		String connectionUrl = "jdbc:sqlserver://127.0.0.1:1434;databaseName=loboticket";
+		try (Connection con = DriverManager.getConnection(connectionUrl,"Steve","password");) {
 
 			System.out.println(con.getClass());
 
